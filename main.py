0b2c34fa342cc0fc44baed3ccf971e565964ed8c
Jonathon-AR <74187337+Jonathon-AR@users.noreply.github.com>
2025-04-20 06:02:18 +0530

    commit function

--- main.py
+++ main.py
@@ -358,3 +358,54 @@ def kvlm_serialize(kvlm):
             ret += k + b" " + (v.replace(b"\n", b"\n ")) + b"\n"
     ret += b"\n" + kvlm[b""]
     return ret
+
+class GitCommit(GitObject):
+    fmt = b'commit'
+
+    def deserialize(self, data):
+        self.kvlm = kvlm_parse(data)
+
+    def serialize(self):
+        return kvlm_serialize(self.kvlm)
+
+# log command
+
+
+argsp = argsubparsers.add_parser(
+    "log", help="Display history of a given commit.")
+argsp.add_argument("commit",
+                   default="HEAD",
+                   nargs="?",
+                   help="Commit to start at.")
+
+
+def cmd_log(args):
+    repo = repo_find()
+
+    print("digraph wyaglog{")
+    log_graphviz(repo, object_find(repo, args.commit), set())
+    print("}")
+
+
+def log_graphviz(repo, sha, seen):
+
+    if sha in seen:
+        return
+    seen.add(sha)
+
+    commit = object_read(repo, sha)
+    assert (commit.fmt == b'commit')
+
+    if not b'parent' in commit.kvlm.keys():
+        # Base case: the initial commit.
+        return
+
+    parents = commit.kvlm[b'parent']
+
+    if type(parents) != list:
+        parents = [parents]
+
+    for p in parents:
+        p = p.decode("ascii")
+        print("c_{0} -> c_{1};".format(sha, p))
+        log_graphviz(repo, p, seen)
