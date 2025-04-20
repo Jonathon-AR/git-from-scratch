9564120d5262c7b12421ebad1298104bcf9d4d30
Jonathon-AR <74187337+Jonathon-AR@users.noreply.github.com>
2025-04-20 05:52:07 +0530

    git init done

--- main.py
+++ main.py
@@ -11,3 +11,156 @@ import zlib
 argparser = argparse.ArgumentParser(description="Content tracker")
 argsubparsers = argparser.add_subparsers(title="Commands", dest="command")
 argsubparsers.required = True
+
+
+def main(argv=sys.argv[1:]):
+    args = argparser.parse_args(argv)
+
+    # if args.command == "add":
+    #     cmd_add(args)
+    # elif args.command == "cat-file":
+    #     cmd_cat_file(args)
+    # elif args.command == "checkout":
+    #     cmd_checkout(args)
+    # elif args.command == "commit":
+    #     cmd_commit(args)
+
+
+class GitRepository(object):
+    """
+    A git repository
+    """
+
+    worktree = None
+    gitdir = None
+    conf = None
+
+    def __init__(self, path, force=False):
+        self.worktree = path
+        self.gitdir = os.path.join(path, ".git")
+
+        if not (force or os.path.isdir(self.gitdir)):
+            raise Exception("Not a Git repository %s" % path)
+
+        self.conf = configparser.ConfigParser()
+        cf = repo_file(self, "config")
+
+        if cf and os.path.exists(cf):
+            self.conf.read([cf])
+        elif not force:
+            raise Exception("Configuration file missing")
+
+        if not force:
+            vers = int(self.cong.get("core", "repositoryformatversion"))
+            if vers != 0:
+                raise Exception("Unsupported respository format version %s" % vers)
+
+
+def repo_file(repo, *path, mkdir=False):
+    """
+    repo_path, create directory if not exist
+    """
+    if repo_dir(repo, *path[:-1], mkdir=False):
+        return repo_path(repo, *path)
+
+
+def repo_path(repo, *path):
+    """
+    pwd + path
+    """
+    return os.path.join(repo.path, *path)
+
+
+def repo_dir(repo, *path, mkdir=False):
+    path = repo_path(repo, *path)
+    if os.path.exists(path):
+        if os.path.isdir(path):
+            return path
+        else:
+            raise Exception("Not a directory %s" % path)
+
+    if mkdir:
+        os.makedirs(path)
+        return path
+    else:
+        return None
+
+
+def repo_create(path):
+    """
+    new repo
+    """
+
+    repo = GitRepository(path, True)
+
+    if os.path.exists(repo.worktree):
+        if not os.path.isdir(repo.worktree):
+            raise Exception("%s is not a directory!" % repo.worktree)
+        if os.listdir(repo.worktree):
+            raise Exception("%s is not empty!" % repo.worktree)
+    else:
+        os.makedirs(repo.worktree)
+
+    assert repo_dir(repo, "branches", mkdir=True)
+    assert repo_dir(repo, "objects", mkdir=True)
+    assert repo_dir(repo, "refs", "tags", mkdir=True)
+    assert repo_dir(repo, "refs", "heads", mkdir=True)
+
+    # .git/description
+    with open(repo_file(repo, "description"), "w") as f:
+        f.write(
+            "Unnamed repository; edit this file 'description' to name the repository.\n"
+        )
+
+    # .git/HEAD
+    with open(repo_file(repo, "HEAD"), "w") as f:
+        f.write("ref: refs/heads/master\n")
+
+    # .git/config
+    with open(repo_file(repo, "config"), "w") as f:
+        config = repo_default_config()
+        config.write(f)
+
+    return repo
+
+
+def repo_default_config():
+    ret = configparser.ConfigParser()
+
+    ret.add_section("core")
+    ret.set("core", "repositoryformatversion", "0")
+    ret.set("core", "filemode", "false")
+    ret.set("core", "bare", "false")
+
+    return ret
+
+
+argsp = argsubparsers.add_parser("init", help="Initialize a new, empty repository.")
+argsp.add_argument(
+    "path",
+    metavar="directory",
+    nargs="?",
+    default=".",
+    help="Where to create the repository",
+)
+
+
+def cmd_init(args):
+    repo_create(args.path)
+
+
+# find repo``
+def repo_find(path=".", required=True):
+    path = os.path.realpath(path)
+
+    if os.path.isdir(os.path.join(path, ".git")):
+        return GitRepository(path)
+
+    parent = os.path.realpath(os.path.join(path, ".."))
+
+    if parent == path:
+        if required:
+            raise Exception("No git directory")
+        else:
+            return None
+    return repo_find(parent, required)
