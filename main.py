50c48a59621b9c72cc3f701bee0f69e10acf3a81
Jonathon-AR <74187337+Jonathon-AR@users.noreply.github.com>
2025-04-20 05:58:49 +0530

    writing objects

--- main.py
+++ main.py
@@ -149,7 +149,7 @@ def cmd_init(args):
     repo_create(args.path)
 
 
-# find repo``
+# find repo
 def repo_find(path=".", required=True):
     path = os.path.realpath(path)
 
@@ -164,3 +164,197 @@ def repo_find(path=".", required=True):
         else:
             return None
     return repo_find(parent, required)
+
+
+class GitObject(object):
+    repo = None
+
+    def __init__(self, repo, data=None):
+        self.repo = repo
+
+        if data != None:
+            self.deserialize(data)
+
+    def serialize(self):
+        raise Exception("Unimplemented!")
+
+    def deserialize(self, data):
+        raise Exception("Unimplemented!")
+
+
+def object_read(repo, sha):
+    path = repo_file(repo, "objects", sha[0:2], sha[2:])
+
+    with open(path, "rb") as f:
+        raw = zlib.decompress(f.read())
+
+        x = raw.find(b" ")
+        fmt = raw[0:x]
+
+        y = raw.find(b"\x00", x)
+        size = int(raw[x:y].decode("ascii"))
+        if size != len(raw) - y - 1:
+            raise Exception("Malformed object {0}: bad length".format(sha))
+
+        if fmt == b"commit":
+            c = GitCommit
+        elif fmt == b"tree":
+            c = GitTree
+        elif fmt == b"tag":
+            c = GitTag
+        elif fmt == b"blob":
+            c = GitBlob
+        else:
+            raise Exception(
+                "Unknown type {0} for object {1}".format(fmt.decode("ascii"), sha)
+            )
+
+        return c(repo, raw[y + 1 :])
+
+
+def object_find(repo, name, fmt=None, follow=True):
+    return name
+
+
+def object_write(obj, actually_write=True):
+    data = obj.serialize()
+    result = obj.fmt + b" " + str(len(data)).encode() + b"\x00" + data
+    sha = hashlib.sha1(result).hexdigest()
+
+    if actually_write:
+        path = repo_file(obj.repo, "object", sha[0:2], sha[2:], mkdir=actually_write)
+        with open(path, "wb") as f:
+            f.write(zlib.compress(result))
+    return sha
+
+
+class GitBlob(GitObject):
+    fmt = b"blob"
+
+    def serialize(self):
+        return self.blobdata
+
+    def deserialize(self, data):
+        self.blobdata = data
+
+
+argsp = argsubparsers.add_parser(
+    "cat-file", help="Provide content of repository objects"
+)
+argsp.add_argument(
+    "type",
+    metavar="type",
+    choices=["blob", "commit", "tag", "tree"],
+    help="Specify the type",
+)
+
+argsp.add_argument("object", metavar="object", help="The object to display")
+
+
+def cmd_cat_file(args):
+    repo = repo_find()
+    cat_file(repo, args.object, fmt=args.type.encode())
+
+
+def cat_file(repo, obj, fmt=None):
+    obj = object_read(repo, object_find(repo, obj, fmt=fmt))
+    sys.stdout.buffer.write(obj.serialize())
+
+
+argsp = argsubparsers.add_parser(
+    "hash-object", help="Compute object ID and optionally create blob from file"
+)
+
+argsp.add_argument(
+    "-t",
+    metavar="type",
+    dest="type",
+    choices=["blob", "commit", "tag", "tree"],
+    default="blob",
+    help="Specify the type",
+)
+
+argsp.add_argument(
+    "-w",
+    dest="write",
+    action="store_true",
+    help="Actually write the object into the database",
+)
+
+argsp.add_argument("path", help="Read object from <file>")
+
+
+def cmd_hash_object(args):
+    if args.write:
+        repo = GitRepository(".")
+    else:
+        repo = None
+
+    with open(args.path, "rb") as fd:
+        sha = object_hash(fd, args.type.encode(), repo)
+        print(sha)
+
+
+def object_hash(fd, fmt, repo=None):
+    data = fd.read()
+
+    if fmt == b"commit":
+        obj = GitCommit(repo, data)
+    elif fmt == b"tree":
+        obj = GitTree(repo, data)
+    elif fmt == b"tag":
+        obj = GitTag(repo, data)
+    elif fmt == b"blob":
+        obj = GitBlob(repo, data)
+    else:
+        raise Exception("Unknown type %s!" % fmt)
+
+    return object_write(obj, repo)
+
+
+def kvlm_parse(raw, start=0, dct=None):
+    if not dct:
+        dct = collections.OrderedDict()
+    spc = raw.find(b" ", start)
+    nl = raw.find(b"\n", start)
+
+    if (spc < 0) or (nl < spc):
+        assert nl == start
+        dct[b""] = raw[start + 1 :]
+        return dct
+
+    key = raw[start:spc]
+
+    end = start
+    while True:
+        end = raw.find(b"\n", end + 1)
+        if raw[end + 1] != ord(" "):
+            break
+
+    value = raw[spc + 1 : end].replace(b"\n ", b"\n")
+
+    if key in dct:
+        if type(dct[key]) == list:
+            dct[key].append(value)
+        else:
+            dct[key] = [dct[key], value]
+    else:
+        dct[key] = value
+
+    return kvlm_parse(raw, start=end + 1, dct=dct)
+
+
+def kvlm_serialize(kvlm):
+    ret = b""
+
+    for k in kvlm.keys():
+        if k == b"":
+            continue
+        val = kvlm[k]
+        if type(val) != list:
+            val = [val]
+
+        for v in val:
+            ret += k + b" " + (v.replace(b"\n", b"\n ")) + b"\n"
+    ret += b"\n" + kvlm[b""]
+    return ret
